87beade81c529a264b9a3a92a78adcaede54dceb
KimBitrus26 <bitruschoji4real@gmail.com>
2024-01-26 19:27:11 +0100

    first commit

--- core/models.py
+++ core/models.py
@@ -8,19 +8,17 @@ class GeoPoint(models.Model):
     A model to utilize postgis
     
     """
+
     name = models.CharField(max_length=255)
     location = models.PointField()
 
     def save(self, *args, **kwargs):
 
         if not self.pk:
-            # Access latitude and longitude from the location field
+            
             longitude, latitude = self.location.coords
-
-            # Create a Point object from the provided coordinates
             point = Point(longitude, latitude)
 
-            # Set the location field with the Point object
             self.location = point
 
         super().save(*args, **kwargs)
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,2 @@
 
 from django.urls import path
-from .views import (index, )
-
-urlpatterns = [
-    
-    path('', index, name="home"),
-
-]
